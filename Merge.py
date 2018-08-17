f7ffd7c815cef901baf44de72b9bc1daba9cfa0d
Vivian Hong <vivianhong925@gmail.com>
2018-08-17 11:55:54 -0400

    Missing check fix

--- Merge.py
+++ Merge.py
@@ -3,6 +3,9 @@ import os
 
 def merge(filename, fileList):
 
+	if not os.path.exists("Desktop/Reports"):
+	os.makedirs("Desktop/Reports")
+
 	with open('Desktop/Reports/' + filename, 'w') as csvOutput:
 		writer = csv.writer(csvOutput) # merged list will end up here
 		writer.writerow(['Name', 'Activity', 'Candidate', 'Job', 'Creation time']) 
